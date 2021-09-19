6444bbb9eb01beb438a407a5220f4435d7cc185f
B6Infinity <broteendas.official@gmail.com>
2021-09-19 15:39:54 +0530

    init git auth now

--- automator/views.py
+++ automator/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-def loginorsignup(request):
-    return render(request, 'loginorsignup.html')
+def login(request):
+    return render(request, 'login.html')
