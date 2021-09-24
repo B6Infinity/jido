67abdb1574301d4a07ec9f4468c89091c65b7e19
B6Infinity <broteendas.official@gmail.com>
2021-09-24 13:27:53 +0530

    sheesh im an css expert!

--- automator/models.py
+++ automator/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    github_username = models.CharField(max_length=39, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.user) + f" - {self.github_username}"
